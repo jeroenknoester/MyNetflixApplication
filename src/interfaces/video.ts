502fd6818c3f72465d244b2b99fb0d4bd3408d0c
Jeroen Knoester <jeroen.knoester@capgemini.com>
2018-11-03 12:00:23 +0100

    modify readme and add interfaces

---
+++ src/interfaces/video.ts
@@ -0,0 +1,12 @@
+import { IGenre } from './genre';
+
+export interface IVideo {
+    id: number;
+    data:string;
+    url:string;
+    title:string;
+    description:string;
+    genre:IGenre;
+    rating:number;
+
+}
