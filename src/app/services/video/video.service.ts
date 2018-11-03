cdbda2a74862aba58df2fd3f1056f8d44ec52f7a
Jeroen Knoester <jeroen.knoester@capgemini.com>
2018-11-03 12:19:22 +0100

    add videoservice logic

--- src/app/services/video/video.service.ts
+++ src/app/services/video/video.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders } from '@angular/common/http'
+import { IVideo } from 'src/app/interfaces/video';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  videoUrl = 'http://localhost:8080/api/videos';
+  
+  headerDict = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin': '*'
+  };
+
+  requestOptions = {
+    headers: new HttpHeaders(this.headerDict)
+  };
+
+  getVideos(): Observable<IVideo[]> {
+    return this.http.get<IVideo[]>(this.videoUrl, this.requestOptions);
+  }
 }
