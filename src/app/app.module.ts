dafab43fb8b1054a0ff3f648ee0ef8eb1a3ed1fa
Jeroen Knoester <jeroen.knoester@capgemini.com>
2018-11-07 13:35:56 +0100

    add actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { VideotestcomponentComponent } from './components/videotestcomponent/videotestcomponent.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppComponent } from './app.component';
 import { HeadermenuComponent } from './components/headermenu/headermenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { VideoComponent } from './components/video/video.component';
 import { ThumbnailComponent } from './components/video/thumbnail/thumbnail.component';
+import { VideoComponent } from './components/video/video.component';
+import { VideotestcomponentComponent } from './components/videotestcomponent/videotestcomponent.component';
 import { SafePipe } from './pipes/safe.pipe';
+import { IAppState } from './store/app-state.interface';
+import { rootReducer } from './store/app.reducer';
+
+
 
 @NgModule({
   declarations: [
@@ -21,7 +27,12 @@ import { SafePipe } from './pipes/safe.pipe';
     SafePipe
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    EffectsModule.forRoot([ VideoEffects ]),
+    StoreModule.forRoot<IAppState>(rootReducer),
+    StoreDevtoolsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/store/video/video.actions.ts
+++ src/app/store/video/video.actions.ts
@@ -1,27 +1,51 @@
 import { Action } from '@ngrx/store';
+import { Video } from 'src/app/shared/video.model';
 
-export enum VideosActionTypes {
+export enum VideoActionTypes {
+    GETALL = '[Message] GetAll',
+    GETALL_SUCCESS = '[Message] GetAll Success',
+    GETALL_ERROR = '[Message] GetAll Error',
     ADD = '[Message] Add',
     TOGGLE_STATUS = '[Message] ToggleStatus',
     REMOVE = '[Message] Remove'
 }
 
+export class GetAll implements Action {
+    public readonly type = VideoActionTypes.GETALL;
+
+    constructor() { }
+}
+
+export class GetAllSuccess implements Action {
+    public readonly type = VideoActionTypes.GETALL_SUCCESS;
+
+    constructor(public readonly videos: Video[]) { }
+}
+
+export class GetAllError implements Action {
+    public readonly type = VideoActionTypes.GETALL_ERROR;
+
+    constructor() { }
+}
+
+
+
 export class Add implements Action {
-    public readonly type = VideosActionTypes.ADD;
+    public readonly type = VideoActionTypes.ADD;
 
     constructor(public readonly video: string) { }
 }
 
 export class ToggleStatus implements Action {
-    public readonly type = VideosActionTypes.TOGGLE_STATUS;
+    public readonly type = VideoActionTypes.TOGGLE_STATUS;
 
     constructor(public readonly videoId: number) { }
 }
 
 export class Remove implements Action {
-    public readonly type = VideosActionTypes.REMOVE;
+    public readonly type = VideoActionTypes.REMOVE;
 
     constructor(public readonly videoId: number) { }
 }
 
-export type VideoActionsUnion = Add | ToggleStatus | Remove;
+export type VideoActionsUnion = GetAllSuccess | GetAllError | GetAll | Add | ToggleStatus | Remove;
